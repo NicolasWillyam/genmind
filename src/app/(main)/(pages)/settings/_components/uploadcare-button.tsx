63944ca9b74b1cdc5001e9ed4be74d653b91015f
HoangCoder <93141009+NicolasWillyam@users.noreply.github.com>
2024-03-31 01:55:39 +0700

    First init

---
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import * as LR from "@uploadcare/blocks";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  onUpload: (e: string) => any;
+};
+
+LR.registerBlocks(LR);
+
+const UploadCareButton = ({ onUpload }: Props) => {
+  const router = useRouter();
+  const ctxProviderRef = useRef<
+    typeof LR.UploadCtxProvider.prototype & LR.UploadCtxProvider
+  >(null);
+
+  useEffect(() => {
+    const handleUpload = async (e: any) => {
+      const file = await onUpload(e.detail.cdnUrl);
+      if (file) {
+        router.refresh();
+      }
+    };
+    ctxProviderRef.current.addEventListener(
+      "file-upload-success",
+      handleUpload
+    );
+  }, []);
+
+  return (
+    <div>
+      <lr-config ctx-name="my-uploader" pubkey="e07f126e0f51038c526f" />
+      <lr-file-uploader-regular
+        ctx-name="my-uploader"
+        css-src={`https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.35.2/web/lr-file-uploader-regular.min.css`}
+      />
+      <lr-upload-ctx-provider ref={ctxProviderRef} ctx-name="my-uploader" />
+    </div>
+  );
+};
+
+export default UploadCareButton;
